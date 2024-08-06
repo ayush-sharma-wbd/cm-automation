6aec48367f8c655c9a874cf51d891dca57ac7085
Ayush Sharma <ayush.sharma@wbd.com>
2024-08-06 16:22:11 +0530

    feat: change status to done through GA workflow.

--- scripts/change-status.sh
+++ scripts/change-status.sh
@@ -2,15 +2,19 @@
 
 TICKET_ID=$1
 
-# response=$(curl \
-#   -u "${JIRA_USER_EMAIL}:${JIRA_API_TOKEN}" \
-#   -X POST --data '{"transition":{"id":"11"}}' \
-#   -H 'Accept: application/json' \
-#   "${JIRA_BASE_URL}/rest/api/2/issue/${TICKET_ID}/transitions")
+# Change Transition Status 
+response=$(curl \
+  -u "${JIRA_USER_EMAIL}:${JIRA_API_TOKEN}" \
+  -X POST --data '{"transition":{"id":"31"}}' \
+  -H 'Accept: application/json' \
+  "${JIRA_BASE_URL}/rest/api/2/issue/${TICKET_ID}/transitions")
+
+# Get Transitions
 response=$(curl \
   -u "${JIRA_USER_EMAIL}:${JIRA_API_TOKEN}" \
   -H 'Accept: application/json' \
   "${JIRA_BASE_URL}/rest/api/2/issue/${TICKET_ID}/transitions")
+
 # if echo "$response" | grep -q "\"key\":\"${TICKET_ID}\""; then
 #   echo "::set-output name=exists::true"
 # else
