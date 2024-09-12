b9497238d38d2dd1edbe1aa1da3023a9390da730
Ayush Sharma <ayush.sharma@wbd.com>
2024-09-12 11:22:57 +0530

    chore: pre prod setup check.

--- scripts/update-cm-ticket.sh
+++ scripts/update-cm-ticket.sh
@@ -8,7 +8,7 @@ fi
 
 # Ensure Number Of Parameters Passed In The Script Are Correct
 if [[ $# -ne 2 ]]; then
-  echo "::error::script given $# positional parameters when 2 are required: <ticket ID> "
+  echo "::error::script given $# positional parameters when 2 are required: <ticket ID> <rollback FLAG>"
   exit 1
 fi
 
@@ -16,16 +16,7 @@ TICKET_ID=$1
 ROLLBACK_FLAG=$2
 
 # 1 denotes Rollback
-echo "$ROLLBACK_FLAG"
-
-# source "${GITHUB_ACTION_PATH}/../scripts/check-cm-ticket.sh" "$TICKET_ID"
-# exit_code=$?
-
-# If Ticket Doesn't Exist
-# if [ $exit_code -ne 0 ]; then
-#   echo "Inner script failed with exit_code $exit_code. Exiting."
-#   exit 1
-# fi
+echo "::info::Rollback Flag = $ROLLBACK_FLAG"
 
 # Fetch Status Of Ticket 
 output_json=$(mktemp)
@@ -43,7 +34,7 @@ if [[ $status_code -eq 200 ]]; then
 
   if [[ "$ROLLBACK_FLAG" == "false" ]]; then
     if [[ "$status_name" == "Change Approved" ]]; then
-      echo "The CM Ticket Is In Approved State. Transitioning It Into In Progress Mode ..."
+      echo "::info::The CM Ticket Is In Approved State. Transitioning It Into In Progress Mode ..."
 
       transition_output_json=$(mktemp)
       transition_status_code=$(curl --silent -u "$JIRA_USER_EMAIL:$JIRA_API_TOKEN" \
@@ -54,20 +45,20 @@ if [[ $status_code -eq 200 ]]; then
         "$JIRA_BASE_URL/rest/api/2/issue/$TICKET_ID/transitions")
 
       if [[ $transition_status_code -eq 204 ]]; then
-        echo "Transition Successful !!"
+        echo "::info::Transition Successful !!"
       else
         echo "::error::Transitioning status of the ticket \"$TICKET_ID\" failed with status code $transition_status_code."
         exit 1
       fi
     elif [[ "$status_name" == "In Progress" ]]; then
-      echo "The CM Ticket is already in In Progress Mode."
+      echo "::info::The CM Ticket is already in In Progress Mode."
     else
       echo "::error::The CM Ticket Is Not In Approved State. Please Check !!"
       exit 1
     fi
   else
     if [[ "$status_name" == "In Progress" || "$status_name" == "Live Checks" || "$status_name" == "Done" ]]; then
-      echo "The CM ticket is in an appropriate state for the current operation. Current state: $status_name."
+      echo "::info::The CM ticket is in an appropriate state for the current operation. Current state: $status_name."
       # If Transition Needed, To Be Added Here ...
     else
       echo "::warning::The CM ticket is not in a state suitable for the current operation. Current state: $status_name. Please verify the ticket's state."
